c1de9f73c2da7a8e621603c5f9091ed689ce9272
FvRaffeWURgit initgit add README.mdcommit -m first <frank.vanraffe@wur.nl>
2026-08-13 17:18:47 +0200

    Define relevant data reading

--- pre-processing.R
+++ pre-processing.R
@@ -1,6 +1,12 @@
-### This files pre-processes the data
+### This files pre-processes the data 
+# define CURE environment
+CURE <- Sys.getenv('CURE')
 
-# read in packages
-librar('data.table')
+# define data location
+data.loc <- paste0(CURE, 'Sleeve Kragge/Data/Overall/Sleeve analyses/')
 
 # read in data
+SleeveData <- read.xlsx(paste0(data.loc, 'All_data_sleeves.xlsx'))
+LeachateData <- read.xlsx(paste0(data.loc, 'all_data_sleeves.xlsx'), sheet = 'leachate data')
+LeachateData <- read.xlsx(paste0(data.loc, 'all_data_sleeves.xlsx'), sheet = 'EPC values')
+                        
