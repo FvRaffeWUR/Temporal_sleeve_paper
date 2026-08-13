0cbde5a1c444ec72462f945e38fd5b80bfa11b89
FvRaffeWURgit initgit add README.mdcommit -m first <frank.vanraffe@wur.nl>
2026-08-13 17:24:49 +0200

    Update packages, tweak data reading, set date to correct format

--- pre-processing.R
+++ pre-processing.R
@@ -5,8 +5,23 @@ CURE <- Sys.getenv('CURE')
 # define data location
 data.loc <- paste0(CURE, 'Sleeve Kragge/Data/Overall/Sleeve analyses/')
 
+# call required packages
+packages <- c(
+  'openxlsx',
+  'data.table'
+)
+
+lapply(packages, library, character.only = TRUE)
+
+
 # read in data
-SleeveData <- read.xlsx(paste0(data.loc, 'All_data_sleeves.xlsx'))
-LeachateData <- read.xlsx(paste0(data.loc, 'all_data_sleeves.xlsx'), sheet = 'leachate data')
-LeachateData <- read.xlsx(paste0(data.loc, 'all_data_sleeves.xlsx'), sheet = 'EPC values')
+sleeve <- as.data.table(read.xlsx(paste0(data.loc, 'All_data_sleeves.xlsx')))
+leachate <- as.data.table(read.xlsx(paste0(data.loc, 'All_data_sleeves.xlsx'), sheet = 'Leachate data'))
+epc <- as.data.table(read.xlsx(paste0(data.loc, 'All_data_sleeves.xlsx'), sheet = 'EPC values'))
+
+
+# Correct date format
+sleeve[, date := as.Date(Date, origin = '1899-12-30')]
+leachate[, date := as.Date(Date, origin = '1899-12-30')]
+
                         
